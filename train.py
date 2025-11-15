e11372e9c4fb6d4e6cf98ff2aedb2ca7f0f57e61
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-11-16 00:42:48 +0900

    Add checkpoint loading and saving functionality in training process

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torchvision.utils import save_image
 import numpy as np
 import cv2 as cv
 from PIL import Image
+import os
 
 from network import UNet
 from dataset import UNetDataset
@@ -19,11 +20,13 @@ DATA_PATH  = './data/faces'
 SEG_PATH = '../data/segs'
 MODEL_PATH = './model'
 MODEL_NAME = 'UNet'
+CHECKPOINT_PATH = './model/checkpoint.pth'
 
 INPUT_LEN = 512
 
 LEARNING_RATE = 0.0001
 EPOCH = 50
+START_EPOCH = 0
 
 TRAIN_BATCH_SIZE = 8
 VAL_BATCH_SIZE = 8
@@ -45,13 +48,28 @@ val_loader = DataLoader(validation_dataset, batch_size=VAL_BATCH_SIZE, shuffle=T
 test_loader = DataLoader(test_dataset, batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=True)
 # Build Model :: In: 3x512x512 -> Out: 6x512x512
 model = UNet()
-model.load_state_dict(torch.load('save/UNet.pth'))
 if torch.cuda.is_available():
     model.cuda()
 
 optimizer = optim.Adam(model.parameters(), LEARNING_RATE, weight_decay=1e-5)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.98)
 criterion = nn.BCEWithLogitsLoss()
+
+# Load checkpoint if exists
+if os.path.exists(CHECKPOINT_PATH):
+    print(f'Loading checkpoint from {CHECKPOINT_PATH}...')
+    checkpoint = torch.load(CHECKPOINT_PATH)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    START_EPOCH = checkpoint['epoch'] + 1
+    print(f'Resuming training from epoch {START_EPOCH}')
+else:
+    print('No checkpoint found. Starting training from scratch.')
+    # Load pretrained model if available (only when starting fresh)
+    if os.path.exists('save/UNet.pth'):
+        model.load_state_dict(torch.load('save/UNet.pth'))
+        print('Loaded pretrained model from save/UNet.pth')
 
 def train(epoch):
     model.train()
@@ -78,6 +96,16 @@ def train(epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 int(100. * batch_idx / len(train_loader)), loss.item() / len(data), float(scheduler.get_lr()[0])))
     print('====> Epoch: {} Average loss: {:.8f}'.format(epoch, train_loss / len(train_loader.dataset)))
+
+def save_checkpoint(epoch, model, optimizer, scheduler, path):
+    """Save training checkpoint"""
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+    }, path)
+    print(f'Checkpoint saved at epoch {epoch}')
     
 def validation():
     model.eval()
@@ -95,10 +123,14 @@ def validation():
         
     print('====> Test set loss: {:.8f}'.format(val_loss / len(val_loader.dataset)))
 
-for epoch in range(EPOCH):
+for epoch in range(START_EPOCH, EPOCH):
     train(epoch)
     validation()
     
+    # Save checkpoint every epoch
+    save_checkpoint(epoch, model, optimizer, scheduler, CHECKPOINT_PATH)
+    
+    # Save model weights every epoch
     torch.save(model.state_dict(), MODEL_PATH+'/'+MODEL_NAME+f'_ep{epoch}'+'.pth')
     
 
